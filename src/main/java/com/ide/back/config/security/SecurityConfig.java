7a322e1ef8f831130974ad81e8d8d17039a22097
dev-len <lightene.dev@gmail.com>
2024-02-02 17:16:33 +0900

    [IDE-4] security form login disabled

---
+++ src/main/java/com/ide/back/config/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.ide.back.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http.formLogin(FormLoginConfigurer::disable);
+
+
+        return http.build();
+    }
+}
