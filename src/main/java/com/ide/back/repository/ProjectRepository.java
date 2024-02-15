f9451420d71e93dd9baeb04009bb7e500dfc8aa1
s-kang <seok550264@gmail.com>
2024-02-15 16:11:28 +0900

    [IDE-28] ProjectRepository 추가

---
+++ src/main/java/com/ide/back/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.ide.back.repository;
+
+import com.ide.back.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+}
