fc4ae55fb743f608a9c8236598dfcefe1706e883
s-kang <seok550264@gmail.com>
2024-02-19 14:02:06 +0900

    [IDE-28] ProjectNotFoundException 메서드 추가

--- src/main/java/com/ide/back/exception/ProjectNotFoundException.java
+++ src/main/java/com/ide/back/exception/ProjectNotFoundException.java
@@ -1,2 +1,11 @@
-package com.ide.back.exception;public class ProjectNotFoundException {
+package com.ide.back.exception;
+
+import com.ide.back.api.response.ApiError;
+import com.ide.back.shared.type.ApiErrorType;
+
+public class ProjectNotFoundException extends IdeApplicationException{
+
+    public ProjectNotFoundException(String message) {
+        super(new ApiError(ApiErrorType.NOT_FOUND, "PROJECT_NOT_FOUND", "Project not found: "  + message));
+    }
 }
