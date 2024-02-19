ea22b14d6a1b92645af4eb70156f0e21be9b9ff3
s-kang <seok550264@gmail.com>
2024-02-19 11:01:16 +0900

    [IDE-28] ProjectDto 수정

--- src/main/java/com/ide/back/dto/projectDto.java
+++ src/main/java/com/ide/back/dto/projectDto.java
@@ -1,17 +1,27 @@
 package com.ide.back.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.ide.back.domain.Project;
+import lombok.*;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Setter
+import java.time.LocalDateTime;
+
+
 @Getter
+@Setter
+@NoArgsConstructor
 public class projectDto {
     private String projectName;
     private String description;
     private String owner;
     private String author;
+
+    public Project toEntity() {
+        return Project.builder()
+                .projectName(projectName)
+                .description(description)
+                .owner(owner)
+                .author(author)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
 }
