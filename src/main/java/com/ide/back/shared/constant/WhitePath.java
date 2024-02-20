6364310a140fcdd39afce7f02ce6e5cfa080b35f
polarbear <sons1998@naver.com>
2024-02-20 22:03:38 +0900

    [IDE-55] jwt 및 시큐리티 설정 수정

--- src/main/java/com/ide/back/shared/constant/WhitePath.java
+++ src/main/java/com/ide/back/shared/constant/WhitePath.java
@@ -2,6 +2,6 @@ package com.ide.back.shared.constant;
 
 public class WhitePath {
     public static final String[] WHITE_LIST = {
-            "/account/register","/authenticated"
+            "/account/register","/authenticated","/api/auth/signUp"
     };
 }
