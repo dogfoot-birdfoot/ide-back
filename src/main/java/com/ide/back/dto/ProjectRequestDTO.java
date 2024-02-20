02a7dc97a92a8d022fe114a471a8b0c7de8ebb4c
s-kang <seok550264@gmail.com>
2024-02-20 16:09:24 +0900

    [IDE-28] Project DTO 삭제 및 수정

--- src/main/java/com/ide/back/dto/ProjectRequestDTO.java
+++ src/main/java/com/ide/back/dto/ProjectRequestDTO.java
@@ -1,5 +1,6 @@
 package com.ide.back.dto;
 
+import com.ide.back.domain.Member;
 import com.ide.back.domain.Project;
 import lombok.*;
 
@@ -17,13 +18,12 @@ public class ProjectRequestDTO {
     private String owner;
     private String author;
 
-    public Project toEntity() {
+    public Project toEntity(Member user) {
         return Project.builder()
                 .projectName(this.projectName)
                 .description(this.description)
                 .owner(this.owner)
                 .author(this.author)
-                .createdAt(LocalDateTime.now())
                 .build();
     }
 }
--- src/main/java/com/ide/back/dto/ProjectResponseDTO.java
+++ src/main/java/com/ide/back/dto/ProjectResponseDTO.java
@@ -8,40 +8,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Data
-@Builder
 public class ProjectResponseDTO {
     private Long id;
-    private Long userId;
     private String projectName;
     private String description;
     private LocalDateTime createdAt;
     private String owner;
     private String author;
-
-    public static ProjectResponseDTO fromEntity(Project project) {
-        if (project == null) {
-            return null; // Return null if the project is null
-        }
-
-        Long userId = null;
-        if (project.getUser() != null) {
-            userId = project.getUser().getId();
-        }
-
-        return ProjectResponseDTO.builder()
-                .id(project.getId())
-                .userId(project.getUser().getId()) // Assuming getUser() returns a non-null Member object with getId() method
-                .projectName(project.getProjectName())
-                .description(project.getDescription())
-                .owner(project.getOwner())
-                .author(project.getAuthor())
-                .createdAt(project.getCreatedAt())
-                .build();
-    }
-
-    public static List<ProjectResponseDTO> fromEntities(List<Project> projects) {
-        return projects.stream().map(ProjectResponseDTO::fromEntity).collect(Collectors.toList());
-    }
 }
 
 
