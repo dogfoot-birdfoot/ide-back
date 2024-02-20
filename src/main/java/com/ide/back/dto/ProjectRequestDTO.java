985d428acc39f3291c01391a7f35512d9e2e15d8
s-kang <seok550264@gmail.com>
2024-02-20 11:33:53 +0900

    [IDE-28] ProjectRequestDTO, ProjectResponseDTO 추가 및 수정

---
+++ src/main/java/com/ide/back/dto/ProjectRequestDTO.java
@@ -0,0 +1,29 @@
+package com.ide.back.dto;
+
+import com.ide.back.domain.Project;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class ProjectRequestDTO {
+    private Long userId;
+    private String projectName;
+    private String description;
+    private String owner;
+    private String author;
+
+    public Project toEntity() {
+        return Project.builder()
+                .projectName(this.projectName)
+                .description(this.description)
+                .owner(this.owner)
+                .author(this.author)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+}
