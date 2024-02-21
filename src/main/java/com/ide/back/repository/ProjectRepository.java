485be470e627c510e7982ff90def4ad2087a70bc
s-kang <seok550264@gmail.com>
2024-02-21 14:10:31 +0900

    [IDE-28] ProjectRepogitory 수정

--- src/main/java/com/ide/back/repository/ProjectRepository.java
+++ src/main/java/com/ide/back/repository/ProjectRepository.java
@@ -1,10 +1,13 @@
 package com.ide.back.repository;
 
+import com.ide.back.domain.Member;
 import com.ide.back.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-
+    List<Project> findByUser(Member user);
 }
