04c5e0d5b714c453c5698a4d7078049f8c501855
s-kang <seok550264@gmail.com>
2024-02-23 09:15:38 +0900

    [IDE-59] 프로젝트멤버 기능 수정

--- src/main/java/com/ide/back/repository/ProjectMemberRepository.java
+++ src/main/java/com/ide/back/repository/ProjectMemberRepository.java
@@ -4,12 +4,20 @@ import com.ide.back.domain.Member;
 import com.ide.back.domain.Project;
 import com.ide.back.domain.ProjectMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
 @Repository
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
     boolean existsByUserAndProject(Member user, Project project);
     List<ProjectMember> findByProjectId(Long projectId);
+
+    @Query("SELECT pm FROM ProjectMember pm WHERE pm.project = :project AND pm.user = :member")
+    ProjectMember findByProjectAndUser(@Param("project") Project project, @Param("member") Member member);
+
+    List<Long> findProjectIdsByUserId(Long userId);
+
+    List<ProjectMember> findByProject(Project project);
 }
